6000a746312758e34b861bcd975b36f53fbc33c4
Sebastian Kristensen Foder <seba127c@edu.campusvejle.dk>
2023-11-29 13:31:39 +0100

    Added form, bootstrap-icons and Changes to model

--- Tubrbokart.Presentation.Websites.TurbokartInternal/Models/Viewmodels/IndexModel.cs
+++ Tubrbokart.Presentation.Websites.TurbokartInternal/Models/Viewmodels/IndexModel.cs
@@ -6,8 +6,8 @@ namespace Tubrbokart.Presentation.Websites.TurbokartInternal.Models.Viewmodels
 {
     public class IndexModel
     {
-        public DateTime Date { get; set; }
-        public int DaysAhead {  get; set; }
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public int DaysAhead { get; set; } = 7;
         public List<Booking> Bookings { get; set; }
     }
 }
