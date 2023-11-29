475cf2789e360a4302517de14a607abb2198145f
Sebastian Kristensen Foder <seba127c@edu.campusvejle.dk>
2023-11-29 12:00:38 +0100

    Added index model

---
+++ Tubrbokart.Presentation.Websites.TurbokartInternal/Models/Viewmodels/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Turbokart.Domain.Entities;
+
+namespace Tubrbokart.Presentation.Websites.TurbokartInternal.Models.Viewmodels
+{
+    public class IndexModel
+    {
+        public DateTime Date { get; set; }
+        public int DaysAhead {  get; set; }
+        public List<Booking> Bookings { get; set; }
+    }
+}
