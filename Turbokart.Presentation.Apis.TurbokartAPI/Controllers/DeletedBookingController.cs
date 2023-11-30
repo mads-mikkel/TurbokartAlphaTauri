cb1282191a60ded0e202a9cf4ae70c5b21df75a5
Phoebe Maul Árnadóttir <andr84j7@edu.campusvejle.dk>
2023-11-30 09:44:39 +0100

    Added DeletedBookingController

---
+++ Turbokart.Presentation.Apis.TurbokartAPI/Controllers/DeletedBookingController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Turbokart.Domain.Entities;
+
+namespace Turbokart.Presentation.Apis.TurbokartAPI.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class DeletedBookingController : Controller
+    {
+        private readonly IDeletedBookingUseCase deletedBookingUseCase;
+
+        public DeletedBookingController( IDeletedBookingUseCase deletedBookingUseCase)
+        {
+            this.deletedBookingUseCase = deletedBookingUseCase;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Booking>>> GetAllDeletedBookings()
+        {
+            try
+            {
+                var deletedBookings = await deletedBookingUseCase.GetAllDeletedBookings();
+                return Ok(deletedBookings);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Booking>> GetOneDeletedBooking(int id)
+        {
+            try
+            {
+                var deletedBooking = await deletedBookingUseCase.GetOneDeletedBooking(id);
+                if (deletedBooking is null)
+                {
+                    return NotFound("Could not find a deleted booking with the id!");
+                }
+                return Ok(deletedBooking);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<IEnumerable<Booking>>> DeleteDeletedBooking(int id)
+        {
+            try
+            {
+                var deletedBooking = await deletedBookingUseCase.DeleteDeletedBooking(id);
+                return Ok(deletedBooking);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
