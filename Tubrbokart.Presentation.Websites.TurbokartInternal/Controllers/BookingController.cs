cfc82dfe6fcf78304ba7df61f1c15d026d1684b5
Sebastian Kristensen Foder <seba127c@edu.campusvejle.dk>
2023-11-30 11:24:17 +0100

    Added Edit functionality

--- Tubrbokart.Presentation.Websites.TurbokartInternal/Controllers/BookingController.cs
+++ Tubrbokart.Presentation.Websites.TurbokartInternal/Controllers/BookingController.cs
@@ -2,6 +2,7 @@
 using Tubrbokart.Presentation.Websites.TurbokartInternal.Models.Viewmodels;
 using Turbokart.Application.Interfaces;
 using Turbokart.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tubrbokart.Presentation.Websites.TurbokartInternal.Controllers
 {
@@ -28,6 +29,27 @@ namespace Tubrbokart.Presentation.Websites.TurbokartInternal.Controllers
                 Time = booking.Date.ToString("hh:mm"),
                 Amount = (byte)booking.Amount
             };
+            return View(model);
+        }
+
+        [HttpPost("Edit/{id}")]
+        public async Task<ActionResult<EditModel>> Edit(int id, [FromForm]EditModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Booking booking = await bookingUseCase.GetOneBooking(id);
+                booking.Grandprix = model.Grandprix;
+                booking.Email = model.Email;
+                booking.Phonenumber = model.Phonenumber;
+                string[] timeParts = model.Time.Split(':');
+                if (timeParts.Length == 2 && int.TryParse(timeParts[0], out int hour) && int.TryParse(timeParts[1], out int minute))
+                {
+                    booking.Date = model.Date.ToDateTime(new TimeOnly().AddHours(hour).AddMinutes(minute));
+                }
+                booking.Amount = model.Amount;
+                await bookingUseCase.UpdateBooking(booking);
+                return RedirectToAction("Index", "Home");
+            }
             return View(model);
         }
 
