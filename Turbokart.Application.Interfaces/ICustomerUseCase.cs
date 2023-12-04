e2dcbdd8caeaf558b0fd1b231f69de6b6a8c32c5
Sebastian Kristensen Foder <seba127c@edu.campusvejle.dk>
2023-12-04 11:54:28 +0100

    Added CustomerUseCase

---
+++ Turbokart.Application.Interfaces/ICustomerUseCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turbokart.Domain.Entities;
+
+namespace Turbokart.Application.Interfaces
+{
+    public interface ICustomerUseCase
+    {
+        Task<Customer> GetCustomer(int id);
+        Task<IEnumerable<Customer>> GetAllCustomers();
+    }
+}
